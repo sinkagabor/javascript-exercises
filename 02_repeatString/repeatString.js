2e4d0a4a3fb2dab48b2a99545011c83808b6e3ce
Gabor Sinka <sinkagabor97@gmail.com>
2023-05-16 16:23:59 +0200

    Solved from exercises 01 to 03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(stringToRepeat, num) {
+    if (num < 0) return "ERROR";
+    let returnedString = "";
+   
+    for(let i=0; i<num; i++){
+        returnedString += stringToRepeat;
+    }
+    return returnedString;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(toBeReversed) {
+    
+    return toBeReversed.split("").reverse().join("");
 };
 
 // Do not edit below this line
