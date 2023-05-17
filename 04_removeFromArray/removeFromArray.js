6666779643ccb9bba863cecc3ca49d55586d8df5
Gabor Sinka <sinkagabor97@gmail.com>
2023-05-17 11:02:53 +0200

    Solved from exercises 04 to 07

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(removeFromThis, ...manyArgs) {
+    const removedArray = removeFromThis;
+    const len = removeFromThis.length;
+    const argLen = manyArgs.length;
+    for(let i=0; i<len; i++){
+        for(let j = 0; j< argLen; j++)
+        {
+            if(removedArray[i] === manyArgs[j]){
+                removedArray.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    return removedArray;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+function isNumber(b)
+{
+    return isNaN(b)||typeof(b)!=="number"?false:true;
+}
 
+const sumAll = function(first, last) {
+    let result = 0;
+
+    if(!isNumber(first) || !isNumber(last) || first < 0 || last < 0){
+        return "ERROR";
+    }
+
+    if(first > last){
+        let temp = last;
+        last = first;
+        first = temp;
+    } 
+
+    let x = +first;
+    let y = +last;
+    let i = x;
+
+    while(i>= x && i<=y){
+        result +=i;
+        i++;
+    }
+    return result;
 };
 
 // Do not edit below this line
